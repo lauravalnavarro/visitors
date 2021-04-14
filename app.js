ae3dae986877a26820655e55aa916b5fd58540d1
Laura <lvna.95@gmail.com>
2021-04-14 16:06:45 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost:27017/visitor', { useNewUrlParser: true });
+mongoose.connection.on("error", function(e) { console.error(e); });
+
+// definimos el schema
+const visitorSchema = mongoose.Schema({
+  name: String,
+  date: { type: Date}
+});
+
+// definimos el modelo
+const Visitor = mongoose.model("Visitor", visitorSchema);
+
+app.get('/', (req, res) =>{
+    Visitor.create({name: req.query.name, date: Date.now()}, function(err){
+        if(err) return console.error(err);
+    });
+    res.send(`<h1>El visitante fue almacenado con éxito</h1>`);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
