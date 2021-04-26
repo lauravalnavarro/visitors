f514cf43a442a5f69797a2ee1b331192d0025dca
Laura <lvna.95@gmail.com>
2021-04-26 16:17:50 -0500

    recurrentes final

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const { urlencoded, bodyParser } = require("body-parser");
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
-app.use(bodyParser, urlencoded({extended: true}));
 
 mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost:27017/visitor', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 
 // definimos el schema
-const schema = mongoose.Schema({
-  name: {type: String, default: 'Anónimo'},
-  count: { type: Number, default: 1}
+const visitorSchema = mongoose.Schema({
+  name: 
+    {type: String, 
+    default: 'Anónimo'},
+  count: 
+    {type: Number, 
+      default : 1}
 });
 
 // definimos el modelo
-var Visitor = mongoose.model('Visitor', schema);
+const Visitor = mongoose.model("Visitor", visitorSchema);
 
-app.get('/', async (req, res) => {
-  const visitorName = req.query.name;
-  const existVisitor = await Visitor.findOne({name: visitorName}).catch(error => console.log(error));
-  
-  if (existVisitor && visitorName){
-    await countVisits(visitorName).catch(error => console.log(error)); 
+app.get('/', async (req, res) =>{
+  const currentVisitor = await Visitor.findOne({name: req.query.name});
+
+  if (currentVisitor && currentVisitor.name !== 'Anónimo'){
+      currentVisitor.count += 1;
+      currentVisitor.save();
   } else {
-    await Visitor.create({name: visitorName}); 
-  }
-
-  await Visitor.find(function(err, visitors){
-    if(err) return console.error(err, visitors);
-    console.log("pintando" + visitors);
-    res.render('index', {visitors: visitors});
-  });
-
-  
+      await Visitor.create({name: req.query.name || 'Anónimo'});
+  }   
+  const visitors = await Visitor.find();
+  return res.render('index', {
+    pageTitle: 'Visitantes recurrentes',
+    visitors});
 });
 
-function countVisits(visitorName){
-  Visitor.findOne({name: visitorName}, function(err, visitor){
-    if(err) return console.error(err);
-    visitor.count +=1;
-    visitor.save(function(err){
-      if (err) return console.log(err);
-    });
-  })
-}
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
